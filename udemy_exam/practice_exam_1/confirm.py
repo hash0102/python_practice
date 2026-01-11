6577a12c88e0d353a3c8cc25767410e4f48274cc
SHota Hashimoto <ha4mo10shota.okc@gmail.com>
2026-01-11 18:13:37 +0900

    exam_1 2回目結果：90%

--- udemy_exam/practice_exam_1/confirm.py
+++ udemy_exam/practice_exam_1/confirm.py
@@ -10,3 +10,11 @@ print(f"pi is {pi:2}")
 print(f"pi is {pi:2f}")
 print(f"pi is {pi:.2f}")
 print(f"pi is {pi:.2}")
+
+print('-'*30)
+
+from dateutil import parser
+
+parser_time = parser.parse("2025-10-30 14:45")
+print(parser_time)
+print(type(parser_time))
