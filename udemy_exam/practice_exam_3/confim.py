628265fa6bcdb29c86201fc417d873aa31cb9cdc
SHota Hashimoto <ha4mo10shota.okc@gmail.com>
2026-01-18 17:59:12 +0900

    exam_3 2回目結果：87%

--- udemy_exam/practice_exam_3/confim.py
+++ udemy_exam/practice_exam_3/confim.py
@@ -7,3 +7,30 @@ lst[0][0] = 10
 print(lst)
 print(copy_lst)
 print(copy_lst2)
+
+print('-'*30)
+
+import unicodedata
+
+s1 = "ガ"
+s2 = "ガ"
+print(s1 == s2)
+
+print(unicodedata.normalize("NFC", s1) == unicodedata.normalize("NFC", s2))
+
+s3 = unicodedata.normalize("NFD", s2)
+print(len(s3))
+for i in s3 :
+    print(i)
+
+print('-'*30)
+
+s4 = "ＡＢＣ①２３"
+s5 = "ABC123"
+print(s4 == s5)
+print(unicodedata.normalize("NFKC", s4) == s5)
+
+s6 = unicodedata.normalize("NFKD",s4)
+print(len(s6))
+for j in s6:
+    print(j)
